ed6e1224e2711e8f4bd5789575ef170da6f94f02
SimplyKyle! <68589851+JustSimplyKyle@users.noreply.github.com>
2023-06-20 21:02:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
+window.open('https://www.digitalocean.com/community/tutorials/how-to-add-javascript-to-html', '_blank').focus();
 window.location.href = urls.length > 0 ? urls[Math.floor(Math.random() * urls.length)] : 'https://github.com/hms5232/random-survey';

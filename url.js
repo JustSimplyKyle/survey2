6ebd3b5ad49849fca01e91643000870690ae8bb4
SimplyKyle! <68589851+JustSimplyKyle@users.noreply.github.com>
2023-07-04 13:56:58 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,10 +8,13 @@
 
 
 const urls = [
-	'https://docs.google.com/forms/d/e/1FAIpQLSdM-YeaCQN_Ei6NLe_cL9_nW34joJ3GE9Sy_kH7IOuzH9KTSQ/viewform?usp=sf_link',
-	'https://docs.google.com/forms/d/e/1FAIpQLSe6C8UyTpb7Xil0SmAf8owfw8on1h9hrDusgLvATGbMay38XQ/viewform?usp=sf_link ',
-	'https://docs.google.com/forms/d/e/1FAIpQLScVG87QXo801X1Tp1yCjDNmDaK0fbbnzdN8_nU9i8CnI6kjEw/viewform?usp=sf_link',
-	'https://docs.google.com/forms/d/e/1FAIpQLSdvdloAlgclcRaLJdOT9s0pxFnvcUXfA9HvC0gbd4o18IBJ6Q/viewform?usp=sf_link',
-	'https://docs.google.com/forms/d/e/1FAIpQLSeoO7rTlT-Rru3hmgrveQatI2AaIThrB8XH5SRvj_lybl14Rw/viewform?usp=sf_link',
-	'https://docs.google.com/forms/d/e/1FAIpQLSc24pxPlzlXGVFfEh6ipb8zdud9yapDIpeG9QeCcucmNiY3VA/viewform?usp=sf_link'
+	'https://reurl.cc/x77YyV',
+	'https://reurl.cc/YeeMyl',
+	'https://reurl.cc/K00yNM',
+	'https://reurl.cc/GAAzWG',
+	'https://reurl.cc/N005V9',
+	'https://reurl.cc/944d2Y',
+	'https://reurl.cc/N005Vk',
+	'https://reurl.cc/VLLGky',
+	'https://reurl.cc/nDDQk6'
 ];
